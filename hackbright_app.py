c12cc4b7b23923ae681358f175f66939a1c1d03b
Student <user@chromebox-004.(none)>
2013-10-10 17:16:01 -0700

    Initial Commit

--- hackbright_app.py
+++ hackbright_app.py
@@ -16,6 +16,38 @@ def connect_to_db():
     CONN = sqlite3.connect("hackbright.db")
     DB = CONN.cursor()
 
+def make_new_student(first_name, last_name, github):
+    query = """INSERT into Students values (?, ?, ?)"""
+    DB.execute(query, (first_name, last_name, github))
+    CONN.commit()
+    print "Successfully added student: %s %s" % (first_name, last_name)
+
+def get_project_by_title(title):
+    query = """SELECT title, description, max_grade FROM Projects WHERE title = ?"""
+    DB.execute(query, (title,))
+    row = DB.fetchone()
+    print """\
+Project Title: %s
+Description: %s
+Maximum Grade: %s""" % (row[0], row[1], row[2])
+
+def make_new_project(title, description, max_grade):
+    query = """INSERT into Projects values (?, ?, ?)"""
+    DB.execute(query, (title, description, max_grade))
+    CONN.commit()
+    print "Successfully added new project: %s" % title
+
+def get_student_grade(first_name, title):
+    query = """SELECT Students.first_name, Grades.project_title, Grades.grade \
+            FROM Students JOIN Grades ON Students.github = Grades.student_github\
+            WHERE Students.first_name = ? AND Grades.project_title = ?"""
+    DB.execute(query, (first_name, title))
+    row = DB.fetchone()
+    print """\
+Student name: %s 
+Project Title: %s 
+Grade: %d""" % (row[0], row[1], row[2])
+
 def main():
     connect_to_db()
     command = None
@@ -29,6 +61,15 @@ def main():
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "title":
+            get_project_by_title(*args)
+        elif command == "new_project":
+            title = tokens[1]
+            description = " ".join(tokens[1:-1])
+            max_grade = tokens[-1]
+            make_new_project(title, description, max_grade)
+        elif command == "get_grade":
+            get_student_grade(*args)
 
     CONN.close()
 
