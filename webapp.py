a516a4c357e3f457c5494b785cf239bf1b0bf679
Student <user@chromebox-004.(none)>
2013-10-15 17:41:54 -0700

    changed get methods to post methods

--- webapp.py
+++ webapp.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def get_github():
-    # hackbright_app.connect_to_db()
-    # projects_list = hackbright_app.get_all_projects()
     return render_template("get_github.html")
 
 @app.route("/student")
@@ -31,40 +29,35 @@ def get_project_grades():
     
     return render_template("project_grades.html", student_grades=rows, title=title, description=description, max_grade=max_grade)
 
-@app.route("/new_student")
+@app.route("/new_student", methods=["POST"])
 def make_new_student():
     hackbright_app.connect_to_db()
-    new_firstname = request.args.get("firstname")
-    new_lastname = request.args.get("lastname")
-    new_github = request.args.get("github")
+    new_firstname = request.form.get("firstname")
+    new_lastname = request.form.get("lastname")
+    new_github = request.form.get("github")
     hackbright_app.make_new_student(new_firstname, new_lastname, new_github)
 
-    return render_template("student_info.html", first_name=new_firstname, last_name=new_lastname, github=new_github)
+    return redirect("/student?github=%s" % new_github)
 
-@app.route("/new_project")
+@app.route("/new_project", methods=["POST"])
 def make_new_project():
     hackbright_app.connect_to_db()
-    title = request.args.get("project_title")
-    description = request.args.get("description")
-    max_grade = request.args.get("max_grade")
+    title = request.form.get("project_title")
+    description = request.form.get("description")
+    max_grade = request.form.get("max_grade")
     hackbright_app.make_new_project(title, description, max_grade)
 
-    return render_template("project_grades.html", title=title, description=description, max_grade=max_grade)
-
-@app.route("/assign_grade")
+    return redirect("/project?project_title=%s" % title) 
+    
+@app.route("/assign_grade", methods=["POST"])
 def assign_grade():
     hackbright_app.connect_to_db()
-    student_github = request.args.get("github")
-    title = request.args.get("title")
-    grade = request.args.get("grade")
+    student_github = request.form.get("github")
+    title = request.form.get("title")
+    grade = request.form.get("grade")
     hackbright_app.assign_grade(student_github, title, grade)
 
     return redirect("/student?github=%s" % student_github)
-
-    # row = hackbright_app.get_student_by_github(student_github)
-    # row2 = hackbright_app.get_student_report(student_github)
-
-    # return render_template("student_info.html", first_name=row[0], last_name=row[1], github=student_github, list_of_grades=row2)
 
 
 if __name__ == "__main__":
